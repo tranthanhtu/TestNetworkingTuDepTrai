990d598649783989758675b2592cc8907c34bd5a
Tran Thanh Tu <tranthanhtuxd2211@gmail.com>
2016-10-19 21:58:52 +0700

    no message

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -13,13 +13,28 @@ post2 = {
     "content" : "Today , she did show up and I felt more lonely than ever"
 }
 
+
+
 posts = [post1,post2]
 
 print(post1["title"])
 
 @app.route('/')
-def hello_world():
+def main():
     return json.dumps(posts)
+
+@app.route('/addpost', methods=["POST"])
+def add_post():
+    args = request.form
+    title = args["title"]
+    content = args["content"]
+    new_post = {
+        "title" : title,
+        "content" : content
+    }
+    posts.append(new_post)
+    print(title, content)
+    return "OK"
 
 
 if __name__ == '__main__':
